3e419f703c6c0eb19ae41cd16c90ab940719a24e
Nagdev-Savita <nagdev.savita@magecomp.com>
2021-11-02 16:13:49 +0530

    Issue solving of delete and massdelete

--- app/code/Magecomp/Imageclean/Controller/Adminhtml/Imageclean/Delete.php
+++ app/code/Magecomp/Imageclean/Controller/Adminhtml/Imageclean/Delete.php
@@ -4,6 +4,7 @@ namespace Magecomp\Imageclean\Controller\Adminhtml\Imageclean;
 
 use Magecomp\Imageclean\Model\ImagecleanFactory;
 use Magento\Backend\App\Action\Context;
+use Magento\Framework\Filesystem\DirectoryList;
 
 class Delete extends AbstractImageclean
 {
@@ -11,31 +12,36 @@ class Delete extends AbstractImageclean
      * @var ImagecleanFactory
      */
     protected $_modelImagecleanFactory;
+    protected $directoryList;
 
     public function __construct(Context $context, 
-        ImagecleanFactory $modelImagecleanFactory)
+        ImagecleanFactory $modelImagecleanFactory,
+        DirectoryList $directoryList)
     {
         $this->_modelImagecleanFactory = $modelImagecleanFactory;
-
+        $this->directoryList = $directoryList;
         parent::__construct($context);
     }
 
     public function execute() {
         if ($this->getRequest()->getParam('id') > 0) 
-		{
+        {
             try 
-			{
+            {
+                $rootPath  =  $this->directoryList->getRoot();
+                $mediaPath =  $rootPath.DIRECTORY_SEPARATOR.'pub'.DIRECTORY_SEPARATOR.'media'.DIRECTORY_SEPARATOR.'catalog'.DIRECTORY_SEPARATOR.'product';
                 $model = $this->_modelImagecleanFactory->create();
                 $model->load($this->getRequest()->getParam('id'));
-                unlink('pub/media/catalog/product'.$model->getFilename());
+                $filename=$model->getFilename();
                 $model->setId($this->getRequest()->getParam('id'))->delete();
-
                 $this->messageManager->addSuccess(__('Image was successfully deleted'));
+                unlink($mediaPath.$filename);
+               
                 $this->_redirect('*/*/');
             } 
-			catch (\Exception $e) 
-			{
-                $this->messageManager->addError($e->getMessage());
+            catch (\Exception $e) 
+            {
+               // $this->messageManager->addError($e->getMessage());
                 $this->_redirect('*/*/edit', ['id' => $this->getRequest()->getParam('id')]);
             }
         }
--- app/code/Magecomp/Imageclean/Controller/Adminhtml/Imageclean/MassDelete.php
+++ app/code/Magecomp/Imageclean/Controller/Adminhtml/Imageclean/MassDelete.php
@@ -7,36 +7,52 @@ use Magento\Backend\App\Action\Context;
 use Magento\Ui\Component\MassAction\Filter;
 use Magecomp\Imageclean\Model\ResourceModel\Imageclean\CollectionFactory;
 use Magecomp\Imageclean\Model\ImagecleanFactory;
+use Magento\Framework\Filesystem\DirectoryList;
 
 class MassDelete extends \Magento\Backend\App\Action
 {
-	protected $filter;
-	protected $collectionFactory;
+    protected $filter;
+    protected $collectionFactory;
     /**
      * @var ImagecleanFactory
      */
     protected $_modelImagecleanFactory;
+    protected $directoryList;
 
-    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory, ImagecleanFactory $modelImagecleanFactory)
+    public function __construct(Context $context, 
+        Filter $filter, 
+        CollectionFactory $collectionFactory,
+         ImagecleanFactory $modelImagecleanFactory,
+        DirectoryList $directoryList)
     {
-		
-		$this->filter = $filter;
+        
+        $this->filter = $filter;
         $this->collectionFactory = $collectionFactory;
         $this->_modelImagecleanFactory = $modelImagecleanFactory;
+        $this->directoryList = $directoryList;
         parent::__construct($context);
     }
 
     public function execute() 
-	{
-		$collection = $this->filter->getCollection($this->collectionFactory->create());
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
         $collectionSize = $collection->getSize();
-
+        $rootPath  =  $this->directoryList->getRoot();
+        $mediaPath = $rootPath.DIRECTORY_SEPARATOR.'pub'.DIRECTORY_SEPARATOR.'media'.DIRECTORY_SEPARATOR.'catalog'.DIRECTORY_SEPARATOR.'product';
         foreach ($collection as $item) 
-		{
-			$model = $this->_modelImagecleanFactory->create();
-			$model->load($item->getImagecleanId());
-            unlink('pub/media/catalog/product'.$model->getFilename());
-            $item->delete();
+        {
+            try {
+                $model = $this->_modelImagecleanFactory->create();
+                $model->load($item->getImagecleanId());
+                $filename = $model->getFilename();
+                $item->delete();
+                unlink($mediaPath . $filename);
+            }
+            catch (\Exception $e)
+            {
+                //$this->messageManager->addError($e->getMessage());
+                $this->_redirect('*/*/edit', ['id' => $item->getImagecleanId()]);
+            }
         }
 
         $this->messageManager->addSuccess(__('A total of %1 image(s) have been deleted.', $collectionSize));
